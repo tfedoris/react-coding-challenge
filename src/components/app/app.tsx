09f317bb4be02a247dcc30afb96d81c2178f2c4f
Tyler Fedoris <tylerfedoris@outlook.com>
2021-08-17 17:47:04 -0400

    Implemented InputTextField component.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,17 @@
+import InputTextField from "components/input-text-field";
 import React from "react";
 import "./app.css";
 
 function App() {
-  return <div className="App"></div>;
+  function handleSubmit() {
+    console.log("Enter key was pressed!");
+  }
+
+  return (
+    <div className="App">
+      <InputTextField onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/input-text-field/input-text-field.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement, useState } from "react";
+
+interface Props {
+  onSubmit: () => void;
+}
+
+function InputTextField({ onSubmit }: Props): ReactElement {
+  const [value, setValue] = useState("");
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
+  function handleKeyDown(event: React.KeyboardEvent) {
+    if (event.key === "Enter") {
+      onSubmit();
+    }
+  }
+
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={handleInputChange}
+      onKeyDown={handleKeyDown}
+    />
+  );
+}
+
+export default InputTextField;
